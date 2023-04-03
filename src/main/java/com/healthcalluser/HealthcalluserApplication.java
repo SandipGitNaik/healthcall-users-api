c62f3185f75465d2e11e6078365ec446ad5a4f06
SandipGitNaik <121107982+SandipGitNaik@users.noreply.github.com>
2023-04-03 10:21:00 +0530

    updated

---
+++ src/main/java/com/healthcalluser/HealthcalluserApplication.java
@@ -0,0 +1,13 @@
+package com.healthcalluser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthcalluserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthcalluserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/healthcalluser/HealthcalluserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthcalluser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthcalluserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
